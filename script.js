f415459c6e2c3a257247c8055b8f5735e0774047
Shahan Ameen <Shahanameen@gmail.com>
2023-10-09 17:10:42 -0500

    displayedpastpresentfuture

--- script.js
+++ script.js
@@ -14,18 +14,39 @@ $(function () {
     // setItem takes 2 arguments - a key, and a value to assign to that key
     localStorage.setItem(noteKey, noteValue)
   })
-
+ // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
   $("#9 textarea").val(localStorage.getItem("9"))
+  $("#10 textarea").val(localStorage.getItem("10"))
+  $("#11 textarea").val(localStorage.getItem("11"))
+  $("#12 textarea").val(localStorage.getItem("12"))
+  $("#13 textarea").val(localStorage.getItem("13"))
+  $("#14 textarea").val(localStorage.getItem("14"))
+  $("#15 textarea").val(localStorage.getItem("15"))
+  $("#16 textarea").val(localStorage.getItem("16"))
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  var currentHour = dayjs().format("H")
+  console.log(currentHour)
+
+  $(".time-block").each(function(){
+    var timeBlockId = $(this).attr("id")
+    //conditional that compares currentHour to timeBlockId, and sets past present or future class to change color
+    if(timeBlockId < currentHour){
+      $(this).addClass("past")
+    }
+    //repeat with else if
+    else if(timeBlockId == currentHour){
+      $(this).addClass("present")
+    }
+  })
+
+ 
+  
   // TODO: Add code to display the current date in the header of the page.
 var currentDate = dayjs().format("MMM D, YYYY	")
 console.log (currentDate)
